061304c82b3934ae89882127d2308e9cc11f7d7c
Andrea Santamaria Garcia <andrea.santamaria@kit.edu>
2022-07-18 11:46:11 +0200

    Fit to function now works

---
+++ preamble.py
@@ -0,0 +1,10 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import set_matplotlib_formats, display
+
+plt.rcParams['figure.figsize'] = 10, 7
+plt.rcParams['savefig.dpi'] = 300
+plt.rcParams['image.cmap'] = "viridis"
+plt.rcParams['image.interpolation'] = "none"
+plt.rcParams['savefig.bbox'] = "tight"
