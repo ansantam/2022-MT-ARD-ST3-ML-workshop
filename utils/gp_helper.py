6a70a888c4e062a7419e7d7af6041e9b53ced0fa
Chenran Xu <chenran.xu@kit.edu>
2022-07-20 16:51:38 +0200

    Add more explanations in BO notebook

--- utils/gp_helper.py
+++ utils/gp_helper.py
@@ -11,6 +11,49 @@ author: Chenran Xu  (chenran.xu@kit.edu)
 """
 
 # plot helper functions
+def plot_gpr_samples(gpr_model: GaussianProcessRegressor, ax, x=np.linspace(0,5,100), n_samples=5):
+    """Plot samples drawn from the Gaussian process model.
+    modified from sklearn example: https://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpr_prior_posterior.html
+
+    If the Gaussian process model is not trained then the drawn samples are
+    drawn from the prior distribution. Otherwise, the samples are drawn from
+    the posterior distribution. Be aware that a sample here corresponds to a
+    function.
+
+    Parameters
+    ----------
+    gpr_model : `GaussianProcessRegressor`
+        A :class:`~sklearn.gaussian_process.GaussianProcessRegressor` model.
+    n_samples : int
+        The number of samples to draw from the Gaussian process distribution.
+    ax : matplotlib axis
+        The matplotlib axis where to plot the samples.
+    """
+    X = x.reshape(-1, 1)
+
+    y_mean, y_std = gpr_model.predict(X, return_std=True)
+    y_samples = gpr_model.sample_y(X, n_samples)
+
+    for idx, single_prior in enumerate(y_samples.T):
+        ax.plot(
+            x,
+            single_prior,
+            linestyle="--",
+            alpha=0.7,
+            label=f"Sample #{idx + 1}",
+        )
+    ax.plot(x, y_mean, color="black", label="Mean")
+    ax.fill_between(
+        x,
+        y_mean - y_std,
+        y_mean + y_std,
+        alpha=0.1,
+        color="black",
+        label=r"$\pm$ 1 std. dev.",
+    )
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+
 def plot_gp(gpr, x, y, x_samples, y_samples, ax=None):
     """Helper function to plot GP posterior
 
@@ -23,7 +66,6 @@ def plot_gp(gpr, x, y, x_samples, y_samples, ax=None):
     if ax is None:
         ax = plt.gcf.add_subplot()
     ax.plot(x, y, label="True f")
-    ax.plot(x_samples, y_samples, "*", label="Noisy Samples")
     y_mean, y_std = gpr.predict(x.reshape(-1, 1), return_std=True)
     ax.plot(x, y_mean, label="GP mean", color='black')
     ax.fill_between(
@@ -34,6 +76,7 @@ def plot_gp(gpr, x, y, x_samples, y_samples, ax=None):
         color="grey",
         label=r"$\pm$ 1 std. dev.",
     )
+    ax.plot(x_samples, y_samples, "*", label="Noisy Samples")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
 
