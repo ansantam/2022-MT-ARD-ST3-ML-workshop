77b59aaf9e8741205f09ca8c106df2e4160b124b
Andrea Santamaria Garcia <andrea.santamaria@kit.edu>
2022-08-02 15:53:24 +0200

    fixing error

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
-git clone https://github.com/ansantam/2022-MT-ARD-ST3-ML-workshop.git
 # sudo apt-get install python3-venv # for linux systems
-cd 2022-MT-ARD-ST3-ML-workshop
 python3 -m venv ML
 source ML/bin/activate
 pip3 install -r requirements.txt
